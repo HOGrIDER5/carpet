545d9eeed822a818ca814ad813caf96d19430fd4
amsta5248 <amsta5248@7HGM6Y1.Student.UGDSB.ED>
2018-10-09 09:30:31 -0400

    java

---
+++ src/carpet/CARPET.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package carpet;
+
+/**
+ *
+ * @author amsta5248
+ */
+public class CARPET {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+        int width= 6;
+        double length= 8.5;
+        double price= 19.95;
+        
+        // algorthim for price
+       double total= width* length* price;
+       
+       
+      System.out.println("The cost for this carpet is going to be"+ total);
+      
+              
+                
+                
+    }
+    
+}
